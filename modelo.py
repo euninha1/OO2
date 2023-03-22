0491bf6d186503022000fad3d442dc246b8a26d0
Fernanda Amaral <54604828+euninha1@users.noreply.github.com>
2023-03-22 11:39:07 -0300

    Herança

---
+++ modelo.py
@@ -0,0 +1,35 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._like = 0
+
+        @property
+        def likes(self):
+            return self._likes
+
+        def dar_likes(self):
+            self._likes += 1
+
+        @property
+        def nome(self):
+            return self._nome
+
+        @nome.setter
+        def nome(self, novo_nome):
+            self._nome = novo_nome.title()
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        self._nome = nome.title()
+        self.ano = ano
+        self.temporada = temporadas
+        self._likes = 0
