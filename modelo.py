b25e2d46e083420ca3d4a5b80afade43327d7870
Fernanda Amaral <54604828+euninha1@users.noreply.github.com>
2023-03-29 10:38:18 -0300

    Criando a classe playlist

--- modelo.py
+++ modelo.py
@@ -39,18 +39,43 @@ class Serie(Programa):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+
+    def tamanho(self):
+        return len(self.programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-listinha = [atlanta, vingadores]
+filmes_e_series = [atlanta, vingadores, demolidor, tmep]
+playlist_fds = Playlist('fim de semana', filmes_e_series)
 
-for programa in listinha:
+
+for programa in filmes_e_series:
     print(programa)
 
 
